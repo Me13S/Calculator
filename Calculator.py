d24321a6a63f02f8ead1cecb86e26cce28cba35f
Me13S <107324602+Me13S@users.noreply.github.com>
2022-06-11 17:10:15 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,58 @@
+print("Enter first number")
+a = int(input())
+print("Enter second number")
+b = int(input())
+
+print ("Choose operation")
+
+print("1.Addition\n2.Subtraction\n3.Multipacation\n4.Divison")
+
+x=input()
+    
+if x== "1":
+    print ("Addition")
+    print (a+b)
+
+elif x== "2":
+    print("Subtraction")
+    print (a-b)
+    
+elif x== "3":
+    print("Multipacation")
+    print (a*b)
+
+elif x=="4":
+    print("Divison")
+    print (a/b)
+    
+
+else:
+    print("invalid")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
